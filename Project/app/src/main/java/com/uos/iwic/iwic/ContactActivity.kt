cc5e2ee67a243bc7846d20d14a817328ad47cc26
James Wood <s192019@uos.ac.uk>
2018-10-17 10:52:17 +0100

    first commit of android project

---
+++ Project/app/src/main/java/com/uos/iwic/iwic/ContactActivity.kt
@@ -0,0 +1,12 @@
+package com.uos.iwic.iwic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+}
