2842cbe6c06cc9783f062a4d14144099bedaeb33
s189385 <s189385@uos.ac.uk>
2018-11-06 22:01:52 +0000

    Created a new android studio project including home page and events template

---
+++ AlexAndroidStudio/app/src/main/java/com/example/alex/iwic/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.alex.iwic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.graphics.Typeface
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
