673f7a2551b99c9ef0c04e997ffe6f0ef1a5ae39
James Wood <s192019@uos.ac.uk>
2018-11-07 10:43:54 +0000

    Update for XML Files
    Merged Alexs Project.

--- Project/app/src/main/java/com/uos/iwic/iwic/HomeActivity.kt
+++ Project/app/src/main/java/com/uos/iwic/iwic/HomeActivity.kt
@@ -2,11 +2,15 @@ package com.uos.iwic.iwic
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+    fun EventsClicked (view:View){
+
     }
 }
