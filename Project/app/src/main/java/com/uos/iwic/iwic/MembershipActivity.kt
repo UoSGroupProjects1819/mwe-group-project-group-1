34dc5af31c91b7c3e6b512292bb26c552c5aec6f
Karina Karushkova <karinadim1998@gmail.com>
2018-12-05 10:11:14 +0000

    Making the payment alternative button and function

--- Project/app/src/main/java/com/uos/iwic/iwic/MembershipActivity.kt
+++ Project/app/src/main/java/com/uos/iwic/iwic/MembershipActivity.kt
@@ -1,11 +1,18 @@
 package com.uos.iwic.iwic
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.system.Os.bind
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.*
 
 class MembershipActivity : Fragment() {
 
@@ -14,7 +21,13 @@ class MembershipActivity : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_membership, container, false)
+        val view = inflater.inflate(R.layout.activity_membership, container, false)
+        val button: Button = view.findViewById(R.id.buttonpayment)
+        button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("http://onlineshop.ucs.ac.uk/product-catalogue/university-services/ipswich-waterfront-innovation-centre")
+            startActivity(intent)
+        }
+        return view
     }
-
 }
