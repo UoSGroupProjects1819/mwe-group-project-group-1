1ef79bed0bf74ced8031ddc30330e07c5c25cadf
s189385 <s189385@uos.ac.uk>
2018-12-11 20:38:31 +0000

    Layout changes to membership and events page

---
+++ Project/app/src/main/java/com/uos/iwic/iwic/aboutus.kt
@@ -0,0 +1,12 @@
+package com.uos.iwic.iwic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class aboutus : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aboutus)
+    }
+}
